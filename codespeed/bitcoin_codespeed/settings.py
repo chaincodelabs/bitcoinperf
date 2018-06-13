208e6d7a944cf4791b067ca16ed62a8242c3a6ab
James O'Beirne <james.obeirne@gmail.com>
2018-06-13 13:02:33 -0400

    Fix settings

--- codespeed/bitcoin_codespeed/settings.py
+++ codespeed/bitcoin_codespeed/settings.py
@@ -10,13 +10,82 @@ DEBUG = True
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 TOPDIR = os.path.split(BASEDIR)[1]
 
+# Codespeed config ------------------------------------------------------------
+
+WEBSITE_NAME = "Bitcoin bench" # This name will be used in the reports RSS feed
+
+DEF_ENVIRONMENT = None # Name of the environment which should be selected as default
+
+DEF_BASELINE = None # Which executable + revision should be default as a baseline
+                    # Given as the name of the executable and commitid of the revision
+                    # Example: defaultbaseline = {'executable': 'myexe', 'revision': '21'}
+
+TREND = 10 # Default value for the depth of the trend
+           # Used by reports for the latest runs and changes view
+
+# Threshold that determines when a performance change over the last result is significant
+CHANGE_THRESHOLD = 3.0
+
+# Threshold that determines when a performance change
+# over a number of revisions is significant
+TREND_THRESHOLD = 5.0
+
+## Changes view options ##
+DEF_EXECUTABLE = 'bitcoind' # Executable that should be chosen as default in the changes view
+                            # Given as the name of the executable.
+                            # Example: defaultexecutable = "myexe"
+
+SHOW_AUTHOR_EMAIL_ADDRESS = True # Whether to show the authors email address in the
+                                 # changes log
+
+## Timeline view options ##
+DEF_BENCHMARK = 'grid' # Default selected benchmark. Possible values:
+                       #   None: will show a grid of plot thumbnails, or a
+                       #       text message when the number of plots exceeds 30
+                       #   "grid": will always show as default the grid of plots
+                       #   "show_none": will show a text message (better
+                       #       default when there are lots of benchmarks)
+                       #   "mybench": will select benchmark named "mybench"
+
+DEF_TIMELINE_LIMIT = 50  # Default number of revisions to be plotted
+                         # Possible values 10,50,200,1000
+
+#TIMELINE_BRANCHES = True # NOTE: Only the default branch is currently shown
+                         # Get timeline results for specific branches
+                         # Set to False if you want timeline plots and results only for trunk.
+
+## Comparison view options ##
+CHART_TYPE = 'normal bars' # The options are 'normal bars', 'stacked bars' and 'relative bars'
+
+NORMALIZATION = False # True will enable normalization as the default selection
+                      # in the Comparison view. The default normalization can be
+                      # chosen in the defaultbaseline setting
+
+CHART_ORIENTATION = 'vertical' # 'vertical' or 'horizontal can be chosen as
+                              # default chart orientation
+
+COMP_EXECUTABLES = None  # Which executable + revision should be checked as default
+                         # Given as a list of tuples containing the
+                         # name of an executable + commitid of a revision
+                         # An 'L' denotes the last revision
+                         # Example:
+                         # COMP_EXECUTABLES = [
+                         #     ('myexe', '21df2423ra'),
+                         #     ('myexe', 'L'),]
+
+USE_MEDIAN_BANDS = True  # True to enable median bands on Timeline view
+
+
+ALLOW_ANONYMOUS_POST = False  # Whether anonymous users can post results
+REQUIRE_SECURE_AUTH = True  # Whether auth needs to be over a secure channel
+# -----------------------------------------------------------------------------
+
+
 #: The directory which should contain checked out source repositories:
 REPOSITORY_BASE_PATH = os.environ.get(
     'REPOSITORY_BASE_PATH', os.path.join(BASEDIR, "repos"))
 
 ALLOWED_HOSTS = ['*']
-
-ALLOW_ANONYMOUS_POST = False
 
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
@@ -49,10 +118,6 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = '{0}.urls'.format(TOPDIR)
-
-CHANGE_THRESHOLD = 0.
-TREND_THRESHOLD = 0.
-
 
 TEMPLATES = [
     {
