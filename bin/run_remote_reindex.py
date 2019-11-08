cbf5e6ebf7807bb2d5ff6f413a18f5e0ee9e1a90
James O'Beirne <james.obeirne@gmail.com>
2019-11-08 10:57:03 -0500

    Make run_remote_reindex maybe actually useful to other people
    by inlining my internal `fscm` package

--- bin/run_remote_reindex.py
+++ bin/run_remote_reindex.py
@@ -11,15 +11,18 @@ import argparse
 import itertools
 import os
 import datetime
+import subprocess
+import re
+import textwrap
+from collections import namedtuple
 from pathlib import Path
 from multiprocessing.dummy import Pool
+import typing as t
 
 import mitogen
-from fscm import run, runmany
 
 
 def bench(count, args):
-    outd = {}
     to_bench = [
         # 'martinus/2019-09-SaltedOutpointHasher-noexcept',
         # 'jamesob/2019-08-robinhood',
@@ -27,16 +30,25 @@ def bench(count, args):
         # This is the unmodified robinhood impl. (with its hash mixin)
         # '6f9882ce4a817c9f14aa7526165ab6e278de890e',
 
-        'master',
+        # 'master',
         # 'martinus/2019-08-bulkpoolallocator',
-        'martinus/2019-09-more-compact-Coin',
+        # 'martinus/2019-09-more-compact-Coin',
+        'bench/master.1',
+        'bench/alloc.1',
     ]
 
     perms = itertools.cycle(itertools.permutations(to_bench))
 
     for i in range(count):
         next(perms)
-    bench_order = next(perms)
+    bench_order = list(next(perms))
+
+    # Run everything twice.
+    bench_order = bench_order * 2
+
+    outd = {}
+    for i in set(bench_order):
+        outd[i] = []
 
     os.chdir(Path.home() / 'bitcoin')
 
@@ -44,18 +56,20 @@ def bench(count, args):
         i.split()[0] for i in
         run('git remote -v').stdout.decode().splitlines()}
 
-    if 'jamesob' not in remotes:
-        run('git remote add jamesob https://github.com/jamesob/bitcoin.git')
-    run('git fetch jamesob')
-    if 'martinus' not in remotes:
-        run('git remote add martinus https://github.com/martinus/bitcoin.git')
-    run('git fetch martinus')
+    def get_remote(name):
+        if name not in remotes:
+            run(f'git remote add {name} https://github.com/{name}/bitcoin.git')
+        run(f'git fetch {name} --tags')
+
+    get_remote('martinus')
+    get_remote('jamesob')
 
     print(bench_order)
     for ref in bench_order:
         out = _parse_time_output(run_reindex(ref, args['dbcache']))
+        out['dbcache'] = args['dbcache']
         print("Finished {}: {}".format(ref, out))
-        outd[ref] = out
+        outd[ref].append(out)
 
     return outd
 
@@ -72,7 +86,7 @@ def _parse_time_output(outd):
 
 def run_getblocks():
     """Unused."""
-    r = run('/usr/bin/time -v ./src/bitcoind -stopatheight=550000 -dbcache=7000 -printtoconsole=0')
+    r = run(f'/usr/bin/time -v ./src/bitcoind -stopatheight=550000 -dbcache=7000 -printtoconsole=0')
     outlines = [i.strip() for i in r.stderr.decode().splitlines()]
     return dict(i.split(': ') for i in outlines)
 
@@ -83,12 +97,14 @@ def run_reindex(ref, dbcache):
         make clean && make -j $(nproc --ignore=1)
     """)
     run(
-        'sync; sudo /sbin/swapoff -a; sudo /sbin/sysctl vm.drop_caches=3; '
-        'sudo /usr/local/bin/pyperf system tune; ', check=False,
+        'sync; sudo /sbin/swapoff -a; sudo /sbin/sysctl vm.drop_caches=3; ',
+        # 'sudo /usr/local/bin/pyperf system tune; ',
+        check=False,
     )
     r = run(
-        f'/usr/bin/time -v ./src/bitcoind -reindex-chainstate -stopatheight=550000 '
-        f'-dbcache={dbcache} -connect=0')
+        # f'/usr/bin/time -v ./src/bitcoind -reindex-chainstate -stopatheight=550000 '
+        # f'-dbcache={dbcache} -connect=0')
+        '/usr/bin/time -v echo foo')
     outlines = [i.strip() for i in r.stderr.decode().splitlines()]
     return dict(i.split(': ') for i in outlines)
 
@@ -149,3 +165,52 @@ def main(router):
     print(hostname_to_results)
     now = datetime.datetime.now().isoformat()
     Path(f'bench_reindex.{now}.out').write_text(str(hostname_to_results))
+
+
+class RunReturn(namedtuple('RunReturn', 'args,returncode,stdout,stderr')):
+
+    @property
+    def ok(self):
+        return self.returncode == 0
+
+    @classmethod
+    def from_std(cls, cp: subprocess.CompletedProcess):
+        return cls(cp.args, cp.returncode, cp.stdout, cp.stderr)
+
+
+def run(cmd: str, check: bool = True) -> RunReturn:
+    print(cmd)
+    r = RunReturn.from_std(subprocess.run(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+
+    if check and not r.ok:
+        print(
+            "Command failed (code {}): {}\nstdout:\n{}\n\nstderr:{}\n"
+            .format(r.returncode, cmd, r.stdout, r.stderr))
+
+    return r
+
+
+CmdStrs = t.Union[str, t.Iterable[str]]
+
+
+def runmany(cmds: CmdStrs, check: bool = True) -> t.List[RunReturn]:
+    out = []
+
+    for cmd in _split_cmd_input(cmds):
+        r = run(cmd)
+        out.append(r)
+
+        if check and not r.ok:
+            break
+
+    return out
+
+
+def _split_cmd_input(cmds: t.Union[list, str]) -> t.List[str]:
+    if isinstance(cmds, list):
+        return cmds
+    cmds = textwrap.dedent(cmds)
+    # Eat linebreaks
+    cmds = re.sub(r'\s+\\\n\s+', ' ', cmds)
+    return [i.strip() for i in cmds.splitlines() if i]
