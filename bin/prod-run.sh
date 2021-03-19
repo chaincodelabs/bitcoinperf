984b5dee2100014eca3bec7b41c5dca70d38fbe9
James O'Beirne <james.obeirne@pm.me>
2021-03-19 11:44:47 -0400

    bin: add retry limit to prod-run.sh

--- bin/prod-run.sh
+++ bin/prod-run.sh
@@ -3,6 +3,11 @@
 #
 # Runs the bitcoinperf benchmarking process on our bench servers.
 #
+# Environment variables:
+#
+#   - BITCOINPERF_RUN_YML: specify which yaml script to execute
+#     (defaults to ./runner/prod.yml)
+#
 
 if ! python3.8 --version >/dev/null; then
   echo "Need to install Python 3.8"
@@ -14,15 +19,31 @@ if ! grep bitcoinperf setup.py >/dev/null; then
   exit 1
 fi
 
-BITCOINPERF_RUN_YML=${BITCOINPERF_RUN_YML:-examples/prod.yml}
-
 git pull
 python3.8 -m pip install -q --user --upgrade -e .
 
-while true; do 
+tries=3
+
+PROD_ENV_FILE=./runner/.env
+
+# Which YAML file to run.
+YAML=${BITCOINPERF_RUN_YML:-./examples/prod.yml}
+
+while [ $tries -gt 0 ]; do
   sudo swapoff -a
-  source runner/.env
   git pull
-  ~/.local/bin/bitcoinperf run $BITCOINPERF_RUN_YML
-  sleep 60
+
+  if [ -f $PROD_ENV_FILE ]; then
+    source $PROD_ENV_FILE
+  else
+    echo "warning: no production env file (${PROD_ENV_FILE}) to source"
+
+  if ! ~/.local/bin/bitcoinperf run ${YAML} ; then
+    # On failure, back off and decrement tries
+    ((tries=tries-1))
+    sleep 60
+  else
+    # Reset the counter each time we hit a success
+    tries=3
+  fi
 done
